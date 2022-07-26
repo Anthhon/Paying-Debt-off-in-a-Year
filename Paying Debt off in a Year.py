f08bef44d6107f10c5870420dc13671768b77426
Anthony S <103778076+Anthhon@users.noreply.github.com>
2022-07-25 21:22:45 -0300

    Add files via upload

---
+++ Paying Debt off in a Year.py
@@ -0,0 +1,67 @@
+'''
+
+MITx 6.00.1xIntroduction to Computer Science and Programming Using Python - Unit 2: Simple Programs /  Problem Set 2
+(Paying Debt off in a Year)
+
+Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
+
+The following variables contain values as described below:
+
+    balance - the outstanding balance on the credit card
+
+    annualInterestRate - annual interest rate as a decimal
+
+    monthlyPaymentRate - minimum monthly payment rate as a decimal
+
+For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining balance.
+Be sure to print out no more than two decimal digits of accuracy - so print:
+
+Remaining balance: 813.41
+
+instead of:
+
+Remaining balance: 813.4141998135 
+
+So your program only prints out one thing: the remaining balance at the end of the year in the format:
+
+Remaining balance: 4784.0
+
+A summary of the required math is found below:
+
+    Monthly interest rate= (Annual interest rate) / 12.0
+    Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+    Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
+    Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+
+We provide sample test cases below. We suggest you develop your code on your own machine, and make sure your code passes the sample test cases,
+before you paste it into the box below. 
+'''
+
+
+balance = 484    # The outstanding balance on the credit card
+annual_interest_rate = 0.2  # Annual interest rate as a decimal
+monthly_payment_rate = 0.04  # Minimum monthly payment rate as a decimal
+
+
+def month_interest_rate():  # Interest rate per month
+    return annual_interest_rate/12
+
+
+def minimum_monthly_payment():  # Minimum payment in the actual month
+    return monthly_payment_rate * balance
+
+
+def monthly_unpaid_balance():   # The outstanding balance on the credit card after the month payment
+    return balance - minimum_monthly_payment()
+
+
+def updated_balance_each_month():   # The outstanding balance on the credit card after sum the interest rate
+    return monthly_unpaid_balance() + (month_interest_rate() * monthly_unpaid_balance())
+
+
+# Calculate the remaining balance after the 12 months, paying just the minimum monthly payment
+for times in range(12):
+    balance = updated_balance_each_month()
+    round_balance = round(balance, 2)
+
+print(f"Remaining Balance: {round_balance}")
